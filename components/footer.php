6911f233c40deca82fcd0e45e1919f32228d6fcf
rim-moghlali <rim-moghlali@laplateforme.io>
2022-12-20 18:37:21 +0100

    Initial commit

---
+++ components/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer of livre-or | Rim Moghlali -->
+<footer>
+  <p>Made by <a href="https://github.com/rim-moghlali/livre-or">
+    <img src="images/github-logo.svg"> <span>rim-moghlali</span></a>
+  </p>
+</footer>
+
